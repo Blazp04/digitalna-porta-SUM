f0c14d967f9d5955329eeffbf0924b9417abc1ac
blazp04 <blaz.peric4@gmail.com>
2024-10-22 11:40:10 +0200

    Dodavanje novog dokumenta

--- admin/src/views/DashbordView.vue
+++ admin/src/views/DashbordView.vue
@@ -6,6 +6,16 @@ import { Button } from '@/components/ui/button';
 import { Textarea } from '@/components/ui/textarea';
 import Documents from '@/components/Documents.vue';
 import {
+    Sheet,
+    SheetClose,
+    SheetContent,
+    SheetDescription,
+    SheetFooter,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from '@/components/ui/sheet'
+import {
     Card,
     CardContent,
     CardHeader,
@@ -111,6 +121,33 @@ async function getDocumentDetails(documentID: number) {
         console.error("Error fetching documents:", error);
     }
 }
+const newDocumentName = ref('');
+const newDocumentText = ref('');
+
+async function createDocument() {
+    const api_key = localStorage.getItem('api_key')
+
+    try {
+        await axios.post(import.meta.env.VITE_API_BASE_URL + '/addData',
+            {
+                text: newDocumentText.value,
+                title: newDocumentName.value
+            },
+            {
+                headers: {
+                    'api-key': api_key,
+                }
+            });
+        getDocuments();
+        newDocumentName.value = '';
+        newDocumentText.value = '';
+    } catch (error) {
+        console.error("Error fetching documents:", error);
+    }
+
+}
+
+
 
 onMounted(async () => {
     await getDocuments()
@@ -118,12 +155,54 @@ onMounted(async () => {
 </script>
 
 <template>
+
+
     <div style="position: fixed; left: 10px; top: 20px; bottom: 10px; width: 300px;">
+
         <Card style="height: calc(100% - 20px)" class="border-2">
             <CardHeader>
                 <CardTitle>Djuro Znanje</CardTitle>
             </CardHeader>
             <CardContent style="overflow-y: auto; height: 100%">
+                <Sheet>
+                    <SheetTrigger as-child>
+                        <Button variant="outline" class="w-full">
+                            Dodaj novi dokument
+                        </Button>
+                    </SheetTrigger>
+                    <SheetContent>
+                        <SheetHeader>
+                            <SheetTitle>Dodaj novi dokument</SheetTitle>
+                            <SheetDescription>
+                                Dodavanjem dokumenta proširuješ znanje koje Đuro ima. Sadržaj dokumenta može se
+                                mijenjati dok naslov ne može
+                            </SheetDescription>
+                        </SheetHeader>
+                        <div class="grid gap-4 py-4">
+                            <div class="grid grid-cols-4 items-center gap-4">
+                                <Label for="name" class="text-right">
+                                    Ime
+                                </Label>
+                                <Input id="name" v-model="newDocumentName" class="col-span-3" />
+                            </div>
+                            <div class="grid grid-cols-4 items-center gap-4">
+                                <Label for="username" class="text-right">
+                                    Znanje
+                                </Label>
+                                <Textarea v-model="newDocumentText" class="col-span-3 flex-1" style="height: 100%"
+                                    id="username" />
+                            </div>
+                        </div>
+                        <SheetFooter>
+                            <SheetClose as-child>
+                                <Button type="button" @click="createDocument">
+                                    Dodaj dokument
+                                </Button>
+                            </SheetClose>
+                        </SheetFooter>
+                    </SheetContent>
+                </Sheet>
+                <div style="height: 10px;"></div>
                 <Documents v-for="(responseItem, index) in response" :key="responseItem.id" :text="responseItem.name"
                     @click="handleClick(index)" :isActive="index === selectedTab" />
             </CardContent>
