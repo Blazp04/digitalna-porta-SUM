3b26984ac683ca3d858fa89504b757a3022c402d
blazp04 <blaz.peric4@gmail.com>
2024-10-23 15:00:32 +0200

    disabeling lasy loading

--- admin/src/router/index.ts
+++ admin/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/LoginView.vue'
+import DashbordView from '../views/DashbordView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,7 +13,8 @@ const router = createRouter({
     {
       path: '/dashboard',
       name: 'dashbord',
-      component: () => import('../views/DashbordView.vue')
+      // component: () => import('../views/DashbordView.vue')
+      component: DashbordView
     },
     {
       path: '/about',
